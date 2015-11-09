e84c726f0d59bbe407efc8f0922450ee115e05d8
thomja <thomja888@hotmail.com>
2015-11-09 10:13:44 +0100

    SimpleTest
    AI klar, jobbar på karta.

---
+++ Assets/Scripts/NavmeshTarget.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavmeshTarget : MonoBehaviour {
+	public GameObject pacman;
+	private NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination(pacman.transform.position);
+	}
+}
