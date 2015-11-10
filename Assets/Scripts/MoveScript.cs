1c424efcf7ac64604f5d6f0958729ec9cb54aae1
thomja <thomja888@hotmail.com>
2015-11-10 11:22:30 +0100

    Something has changed.
    No idea what.

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -3,71 +3,34 @@ using System.Collections;
 
 public class MoveScript : MonoBehaviour {
 	public int myDirection = 0;
-	public float speed;
-	public GameObject[] travDirectionBox;
-	public BoxChecker[] myBoxes;
+	public float speed = 1;
 	// Use this for initialization
 	void Start () {
-		myBoxes[0] = travDirectionBox[0].GetComponent<BoxChecker>();
-		myBoxes[1] = travDirectionBox[1].GetComponent<BoxChecker>();
-		myBoxes[2] = travDirectionBox[2].GetComponent<BoxChecker>();
-		myBoxes[3] = travDirectionBox[3].GetComponent<BoxChecker>();
-		speed = 0.1f;
+
 	}
 
-	void speedReset(){
-		if(speed == 0f){
-			speed = 0.1f;
-		}
-	}
-
-	public int ChangeDirection(int newDirection){
-		if(newDirection == 1 && myBoxes[0].isColliding <= 0){
-			speedReset();
-			return(5);
-		} else if (newDirection == 2 && myBoxes[1].isColliding <= 0){
-			transform.Rotate(0f, 180f, 0f);
-			speedReset();
-			return(5);
-		} else if (newDirection == 3 && myBoxes[2].isColliding <= 0){
-			transform.Rotate(0f, -90f, 0f);
-			speedReset();
-			return(5);
-		} else if (newDirection == 4 && myBoxes[3].isColliding <= 0){
-			transform.Rotate(0f, 90f, 0f);
-			speedReset();
-			return(5);
-		}
-		return(myDirection);
-	}
 
 	// Update is called once per frame
 	void Update () {
-		if(myDirection == 0){
-
-		} else if(myDirection != 6) {
+		if(Input.GetKey(KeyCode.W)){
 			transform.Translate(new Vector3(0f,0f,1f) * speed);
-			myDirection = ChangeDirection(myDirection);
-		}
-		if(Input.GetKeyDown(KeyCode.W)){
-			myDirection = 1;
 		}
 		if(Input.GetKeyDown(KeyCode.S)){
-			myDirection = 2;
+			transform.Rotate(0f, 180f, 0f);
 		}
-		if(Input.GetKeyDown(KeyCode.A)){
-			myDirection = 3;
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(0f, -3f, 0f);
 		}
-		if(Input.GetKeyDown(KeyCode.D)){
-			myDirection = 4;
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(0f, 3f, 0f);
 		}
-	}
-
-	void OnCollisionEnter(Collision myColl){
-		Debug.Log (myBoxes[0].name);
-		if(myColl.gameObject.tag == "Wall" && myBoxes[0].isColliding > 0){
-			speed = 0f;
-			myDirection = 6;
+		if(Input.GetKey(KeyCode.E)){
+			speed = speed / 2;
+			transform.Translate(new Vector3(1f,0f,0f) * speed);
+		}
+		if(Input.GetKey(KeyCode.Q)){
+			speed = speed / 2;
+			transform.Translate(new Vector3(-1f,0f,0f) * speed);
 		}
 	}
 }
