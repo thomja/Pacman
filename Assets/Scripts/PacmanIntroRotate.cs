d549180420e9d683c4724c21f6f0c54e3c68f062
thomja <thomja888@hotmail.com>
2015-11-16 11:32:57 +0100

    Menu Update
    Added Menu and coded Exit and Play buttons

---
+++ Assets/Scripts/PacmanIntroRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacmanIntroRotate : MonoBehaviour {
+	public GameObject pacman;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,1,0);
+	}
+}
