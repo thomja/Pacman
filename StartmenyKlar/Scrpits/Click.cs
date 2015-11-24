2d25b772bc0ad3dbe7c05fe7e611b7de98d333e6
Andreassp15 <hugo_boss_andreas@hotmail.com>
2015-11-24 14:55:17 +0100

    StartmenyKlar

---
+++ StartmenyKlar/Scrpits/Click.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Click : MonoBehaviour
+{
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+    public void OnClickStart()
+    {
+        Application.LoadLevel("Score");
+    }
+    public void OnClickHighscore()
+    {
+        Application.LoadLevel("Highscore");
+    }
+    public void OnClickOptions()
+    {
+        Application.LoadLevel("Options");
+    }
+    public void OnClickStartmenu()
+    {
+        Application.LoadLevel("Startmenu");
+    }
+
+}
---
+++ StartmenyKlar/Scrpits/Highscore.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Highscore : MonoBehaviour
+{
+    public int score;
+    public int[] highscore = new int[10];
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < highscore.Length; i++)
+        {
+            if (score > highscore[i])
+            {
+                
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        PlayerPrefs.Save();
+    }
+}
