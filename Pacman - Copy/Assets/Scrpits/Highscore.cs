e0dfd047a6037a0082d067298aa1cd119b1526b7
Andreassp15 <hugo_boss_andreas@hotmail.com>
2015-11-26 18:31:06 +0100

    Kopia

---
+++ Pacman - Copy/Assets/Scrpits/Highscore.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Highscore : MonoBehaviour
+{
+    public int score;
+    public int[] highscore = new int[10];
+    public Text pointText;
+
+    void Awake()
+    {
+        score = Move.points;
+        PlayerPrefs.SetInt("pointText",0);
+        SetpointText();
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < highscore.Length; i++)
+        {
+            if (score > highscore[i])
+            {
+                
+            }
+        }
+    }
+
+    void SetpointText()
+    {
+        pointText.text = "" + score.ToString();
+    }
+}
---
+++ Pacman - Copy/Assets/Scrpits/Namn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Namn : MonoBehaviour
+{
+    public static string SpelarNamn;
+    public static string namn;
+
+    public void Awake()
+    {
+        PlayerPrefs.SetString("SpelarNamn", "Spelare 1");
+        PlayerPrefs.Save();
+    }
+}
